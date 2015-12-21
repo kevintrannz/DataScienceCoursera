df44b5dbd16ae157ed1d68ef13d9d0a796be6309
mickvanhulst <mick.vanhulst@gmail.com>
2015-12-21 12:58:39 +0100

    Finished Exploratory Data Analysis project week 3

--- Exploratory Data Analysis/Week 3/Course project/plot5.R
+++ Exploratory Data Analysis/Week 3/Course project/plot5.R
@@ -5,14 +5,14 @@ setwd("C:/DataScienceCoursera/Exploratory Data Analysis/Week 3/Course project/")
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-##5. Across the United States, how have emissions from coal combustion-related sources changed from 1999-2008?
+##5. How have emissions from motor vehicle sources changed from 1999-2008 in Baltimore City?
 library(dplyr)
 
 ##filter data, select needed data, group this data by year and type and lastly summarise data so the Emissions get summarized. 
 dataFiltered <- NEI %>%
                     filter(NEI$fips=="24510" & NEI$type=="ON-ROAD") %>%
                         select(Emissions, year) %>%
-                            group_by(year) %>%
+                             group_by(year) %>%
                                 summarise(sum.Emissions = sum(Emissions))
 
 
